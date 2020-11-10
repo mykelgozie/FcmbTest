a5f812d53139ebb0bc692437beb894a3087359d8
michael <mykelgozie@gmail.com>
2020-11-10 17:33:57 +0100

    Initial

---
+++ AtmClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fcmb
+{
+    public class AtmClass
+    {
+
+        public string CheckAtmCard(string num)
+        {
+            var result = "";
+
+            if (num.Length >= 16 && num.Length <= 19)
+            {
+                var mask = "XXXX";
+                var openNumber = num.Substring(4, num.Length - 8);
+                var final = mask + openNumber + mask;
+                result = final;
+            }
+            else
+            {
+                result = "Invalid Atm Card ";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fcmb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var number = "1234567891234567";
+            var card = new  AtmClass();
+            var consealedCard = card.CheckAtmCard(number);
+            Console.WriteLine(consealedCard);
+
+
+
+
+        }
+
+      
+    }
+}
